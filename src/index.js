11ccbf0fa192ca795dd4bd320cab4a74b44b811e
a1exymoroz <a1exy.moroz@gmail.com>
2020-04-30 23:22:52 +0300

    init: movie search

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './styles.scss';
+
+async function start() {
+  return await Promise.resolve('async is working');
+}
+
+start().then(console.log);
+
+const unused = 42;
+
+class Util {
+  static id = Date.now();
+}
+
+console.log('Util Id:', Util.id);
+console.log(unused);
